532acfba2887f311587d77ecb40347681e310259
tls427wodnr <tlswodnr427@naver.com>
2025-03-11 08:41:43 +0900

    fix showMenu function

--- BaseballGame0310/BaseballGame.swift
+++ BaseballGame0310/BaseballGame.swift
@@ -18,8 +18,7 @@ class BaseballGame {
             print("환영합니다! 원하시는 번호를 입력해주세요")
             print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
             
-            if let input = readLine() {
-                let choice = Int(input)
+            if let input = readLine(), let choice = Int(input) {
                 switch choice {
                 case 1:
                     startGame()
