6363884c4a241bf7ccf020c876872155049a49cd
tls427wodnr <tlswodnr427@naver.com>
2025-03-11 17:12:11 +0900

    refactor: replacing `Int(String($0))` with `$0.wholeNumberValue`

--- BaseballGame0310/Service/GameSession.swift
+++ BaseballGame0310/Service/GameSession.swift
@@ -18,10 +18,12 @@ class GameSession {
             tryCount += 1
             print("숫자를 입력하세요")
             if let input = readLine(){
-                let inputNumbers = Array(input).compactMap { Int(String($0)) }
-                // 1. 입력받은 문자열을 문자(Character) 배열로 변환
-                // 2. 각 문자를 String으로 변환한 후 Int로 변환 (문자 자체는 Int로 직접 변환 불가하기 때문)
-                // 3. 변환에 실패한 경우(nil)는 제거하고, 최종적으로 정수 배열을 생성
+                // let inputNumbers = Array(input).compactMap { Int(String($0)) }
+                let inputNumbers = Array(input).compactMap { $0.wholeNumberValue }
+                // 1. 입력받은 문자열을 문자(Character) 배열로 변환 (예: "123a" -> ["1","2","3","a"])
+                // 2. wholeNumberValue를 이용하여 문자 배열을 정수형 배열로 변환 (예: ["1","2","3","a"] -> [1,2,3,nil])
+                // 3. 이때 wholeNumberValue는 숫자가 아닌 값은 nil로 반환 하는데 이 값들을 compactMap으로 자동 제거 (예: [1,2,3,nil] -> [1,2,3])
+                // 4. 최종적으로 숫자만 있는 정수 배열을 생성 (예: [1,2,3])
                 
                 // inputNumbers가 중복되지 않은 3개의 값인지 Set으로 변환하여 검증
                 // Set은 중복되는 값은 제거하는 기능이 있기에 만약 1233 같은 경우 통과를 하게 됨
