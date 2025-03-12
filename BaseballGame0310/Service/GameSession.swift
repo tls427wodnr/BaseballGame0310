9e271f27811382512a78c441e4b542a28abdf947
tls427wodnr <tlswodnr427@naver.com>
2025-03-12 19:09:25 +0900

    refactor: input validation and number extraction logic

--- BaseballGame0310/Service/GameSession.swift
+++ BaseballGame0310/Service/GameSession.swift
@@ -18,17 +18,20 @@ class GameSession {
             tryCount += 1
             print("숫자를 입력하세요")
             if let input = readLine(){
-                // let inputNumbers = Array(input).compactMap { Int(String($0)) }
-                let inputNumbers = Array(input).compactMap { $0.wholeNumberValue }
-                // 1. 입력받은 문자열을 문자(Character) 배열로 변환 (예: "123a" -> ["1","2","3","a"])
-                // 2. wholeNumberValue를 이용하여 문자 배열을 정수형 배열로 변환 (예: ["1","2","3","a"] -> [1,2,3,nil])
-                // 3. 이때 wholeNumberValue는 숫자가 아닌 값은 nil로 반환 하는데 이 값들을 compactMap으로 자동 제거 (예: [1,2,3,nil] -> [1,2,3])
-                // 4. 최종적으로 숫자만 있는 정수 배열을 생성 (예: [1,2,3])
+                //let inputNumbers = Array(input).compactMap { $0.wholeNumberValue }
+                let inputNumbers = input.compactMap { $0.wholeNumberValue }
+                // 1. String은 Sequence 프로토콜을 따르므로, compactMap을 직접 적용할 수 있음.
+                //    즉, String을 문자(Char) 배열로 변환할 필요 없음.
+                // 2. compactMap이 입력받은 문자열의 각 문자(Character)를 순회하며 처리
+                // 3. wholeNumberValue를 적용하여 숫자는 변환하고, 숫자가 아니면 nil 반환
+                // 4. compactMap이 nil을 제거하여 최종적으로 정수 배열을 만듦
+                // 예) "123a" -> ["1","2","3","a"] -> ["1","2","3",nil] -> [1,2,3]
                 
                 // inputNumbers가 중복되지 않은 3개의 값인지 Set으로 변환하여 검증
                 // Set은 중복되는 값은 제거하는 기능이 있기에 만약 1233 같은 경우 통과를 하게 됨
                 // 그걸 방지하기 위해 inputNumbers.count == 3을 추가로 검사하여 입력 값 자체가 3개인지도 확인
-                if inputNumbers.count == 3, Set(inputNumbers).count == 3 {
+                // 추가로 input이 123abc 같은 경우 inputNumbers가 123으로 통과되기에 이를 통과 못하게 조건문을 달아줘야함
+                if inputNumbers.count == input.count, inputNumbers.count == 3, Set(inputNumbers).count == 3 {
                     guard let answer = answer else {
                         print("오류: 정답이 설정되지 않았습니다.")
                         return nil
